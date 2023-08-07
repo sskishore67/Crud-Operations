8f09e9320d60d180f0241b2051b3024a35d3b6ed
Kishore S <sskishore67@gmail.com>
2023-08-07 12:24:26 +0530

    sk

--- Client/src/Componetns/Signup.jsx
+++ Client/src/Componetns/Signup.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './CSS/Signup.css'
 function Signup() {
     return (
-       
             <>
        {/*Navbar */}
        <div className='container-fluid'>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -25,13 +25,17 @@ function Signup() {
   </div>
 </nav>
     </div>
-       {/*Form */}
+       {/*Form for signup */}
             <div className='row container-fluid mt-5'>
       <div className='col-sm-4'></div>
       <div className='col-sm-4'>
       <div class="form-floating mb-3">
   <input type="text" class="form-control" id="floatingInput" />
-  <label for="floatingInput" className='floating-label'>Name</label>
+  <label for="floatingInput" className='floating-label'>FirstName</label>
+</div>
+<div class="form-floating mb-3">
+  <input type="text" class="form-control" id="floatingInput" />
+  <label for="floatingInput" className='floating-label'>LastName</label>
 </div>
       <div class="form-floating mb-3">
   <input type="email" class="form-control" id="floatingInput" />
