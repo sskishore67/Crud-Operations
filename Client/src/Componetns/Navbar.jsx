edc02a1d36b9151c802be5da5ad032414d9cf213
sskishore67 <sskishore67@gmail.com>
2023-09-13 12:08:07 +0530

    sk

--- Client/src/Componetns/Navbar.jsx
+++ Client/src/Componetns/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
   <a class="navbar-brand">
-    <h5>CRUD</h5>
+    <h5 className='text-success'>CRUD OPERATOR</h5>
               </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
