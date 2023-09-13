950e6c93fbdd8c81e42940f0ec7358922f487ebb
sskishore67 <sskishore67@gmail.com>
2023-09-13 11:54:05 +0530

    sk

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,8 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './Componetns/Home';
-import Signup from './Componetns/Signup';
-import Signin from './Componetns/Signin';
 import Edit from './Componetns/Edit';
 import Newuser from './Componetns/Newuser';
 function App() {
@@ -13,8 +11,6 @@ function App() {
  <Routes>
   <Route path='/' element={<Home />}></Route>
   <Route path='/Home' element={<Home />}></Route>
-  <Route path='/Signup' element={<Signup />}></Route>
-  <Route path='/Signin' element={<Signin />}></Route>
   <Route path='/Edit/:id' element={<Edit />}></Route>
   <Route path='/Newuser' element={<Newuser />}></Route>
  </Routes>
--- Client/src/Componetns/Navbar.jsx
+++ Client/src/Componetns/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
   <a class="navbar-brand">
-    <h5>Crud</h5>
+    <h5>CRUD</h5>
               </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
